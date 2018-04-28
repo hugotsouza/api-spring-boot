4bb916e203aa35f3ee7a1bcd3db4435f5fd802fd
Hugo Trindade <hugotrindade.dev@gmail.com>
2018-04-28 17:52:09 -0300

    atualizacao - CategoriaResource

--- src/main/java/com/hugotrindade/carrinho/resources/CategoriaResource.java
+++ src/main/java/com/hugotrindade/carrinho/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.hugotrindade.carrinho.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hugotrindade.carrinho.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		return categorias;
 	}
 }
