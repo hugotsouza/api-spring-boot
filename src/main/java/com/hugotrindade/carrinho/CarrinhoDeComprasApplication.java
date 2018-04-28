faf3ad9e18d2c9bfb709a360551706b3fd332e33
Hugo Trindade <hugotrindade.dev@gmail.com>
2018-04-28 16:09:26 -0300

    Projeto criado

---
+++ src/main/java/com/hugotrindade/carrinho/CarrinhoDeComprasApplication.java
@@ -0,0 +1,12 @@
+package com.hugotrindade.carrinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoDeComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhoDeComprasApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hugotrindade/carrinho/CarrinhoDeComprasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hugotrindade.carrinho;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarrinhoDeComprasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
