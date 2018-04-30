6915cae2732c27fd44e2e64148266c7789c85cd3
Hugo Trindade <hugotrindade.dev@gmail.com>
2018-04-30 16:22:46 -0300

    Criando estado e cidade

--- src/main/java/com/hugotrindade/carrinho/CarrinhoDeComprasApplication.java
+++ src/main/java/com/hugotrindade/carrinho/CarrinhoDeComprasApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.hugotrindade.carrinho.domain.Categoria;
+import com.hugotrindade.carrinho.domain.Cidade;
+import com.hugotrindade.carrinho.domain.Estado;
 import com.hugotrindade.carrinho.domain.Produto;
 import com.hugotrindade.carrinho.repositories.CategoriaRepository;
+import com.hugotrindade.carrinho.repositories.CidadeRepository;
+import com.hugotrindade.carrinho.repositories.EstadoRepository;
 import com.hugotrindade.carrinho.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CarrinhoDeComprasApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CarrinhoDeComprasApplication.class, args);
@@ -42,5 +50,21 @@ public class CarrinhoDeComprasApplication implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(c1, c2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade cid1 = new Cidade(null, "Uberlândia", est1);
+		Cidade cid2 = new Cidade(null, "São Paulo", est2);
+		Cidade cid3 = new Cidade(null, "Campinas", est2);
+		
+		est1.addCidades(cid1);
+		est2.addCidades(cid2, cid3);
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
+		
+		
 	}
 }
