1f2e702e9da2ba179eec8707a67c68ebe455541c
Hugo Trindade <hugotrindade.dev@gmail.com>
2018-05-07 19:09:43 -0300

    Implementando autenticação e geração do token JWT

---
+++ src/main/java/com/hugotrindade/carrinho/services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.hugotrindade.carrinho.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.hugotrindade.carrinho.domain.Cliente;
+import com.hugotrindade.carrinho.repositories.ClienteRepository;
+import com.hugotrindade.carrinho.security.UserSS;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
