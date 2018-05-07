4d81ef68de6f2bc165a007bd923286ea3be75feb
Hugo Trindade <hugotrindade.dev@gmail.com>
2018-05-06 22:42:55 -0300

    Criando email HTML

--- src/main/java/com/hugotrindade/carrinho/services/EmailService.java
+++ src/main/java/com/hugotrindade/carrinho/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.hugotrindade.carrinho.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.hugotrindade.carrinho.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido pedido);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/com/hugotrindade/carrinho/services/PedidoService.java
+++ src/main/java/com/hugotrindade/carrinho/services/PedidoService.java
@@ -63,7 +63,7 @@ public class PedidoService {
 			ip.setPedido(pedido);
 		}
 		itemPedidoRepository.saveAll(pedido.getItens());
-		emailService.sendOrderConfirmationEmail(pedido);
+		emailService.sendOrderConfirmationHtmlEmail(pedido);
 		return pedido;
 	}
 	
