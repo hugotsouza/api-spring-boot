d7224eb33c62314eb81c122f8076361610412288
Hugo Trindade <hugotrindade.dev@gmail.com>
2018-05-08 20:32:01 -0300

    Implementando autorização

--- src/main/java/com/hugotrindade/carrinho/config/SecurityConfig.java
+++ src/main/java/com/hugotrindade/carrinho/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.hugotrindade.carrinho.security.JWTAuthenticationFilter;
+import com.hugotrindade.carrinho.security.JWTAuthorizationFilter;
 import com.hugotrindade.carrinho.security.JWTUtil;
 
 @Configuration
@@ -49,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
--- src/main/java/com/hugotrindade/carrinho/services/UserDetailsServiceImpl.java
+++ src/main/java/com/hugotrindade/carrinho/services/UserDetailsServiceImpl.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.hugotrindade.carrinho.domain.Cliente;
 import com.hugotrindade.carrinho.repositories.ClienteRepository;
 import com.hugotrindade.carrinho.security.UserSS;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Autowired
