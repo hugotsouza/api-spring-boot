a0e099d7a2f5e8bd26a346b662f8fbd31de0b09b
Hugo Trindade <hugotrindade.dev@gmail.com>
2018-05-18 06:06:00 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/hugotrindade/carrinho/domain/Cliente.java
+++ src/main/java/com/hugotrindade/carrinho/domain/Cliente.java
@@ -39,7 +39,6 @@ public class Cliente implements Serializable {
 	@JsonIgnore
 	private String senha;
 	
-	private String imageUrl;
 
 	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
@@ -182,12 +181,6 @@ public class Cliente implements Serializable {
 
 	}
 
-	public String getImageUrl() {
-		return imageUrl;
-	}
 
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
 
 }
